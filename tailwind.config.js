ee42df689bfa56e834867e9dd5348263e92281fb
Kelly Chara <fabianachara@gmail.com>
2022-01-09 17:43:11 -0500

    finish desing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      'sans': ['Roboto'],
+    },
     extend: {
       backgroundImage: {
         'calc': "url('../images/Calculator.png')",
-      }
+      },
+      
 
     },
   },
